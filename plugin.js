946f858ceb0480ccc8eb539df9c524141053ce3a
Bob Gray <gray.bob98@gmail.com>
2018-07-11 01:04:22 -0500

    working on the first working implementation

--- plugin.js
+++ plugin.js
@@ -4,30 +4,124 @@ const vary = require("vary"),
 	defaultOptions = {
 		noStore: false,
 		noCache: false,
-		maxAge: NaN,
+		mustRevalidate: false,
 		public: false,
 		private: false,
-		mustRevalidate: false,
+		maxAge: NaN,
 		vary: []
-	};
+	},
+	quotedETag = /^(?:W\/)?"([^"]+)"$/;
 
 class CacheControl {
-	static create () {
-		return new CacheControl(...arguments);
-	}
-
 	constructor (options = {}) {
 		this.setOptions(options);
 	}
 
-	use (request, response) {
+	async use (request, response) {
 		vary(response, this.options.vary);
+		this.setDirectives(response);
+		await this.setETag(request, response);
+
+		return this.validate(request, response);
 	}
 
 	setOptions (options) {
 		this.options = {...defaultOptions, ...options};
 	}
+
+	setDirectives (response) {
+		var directives = this.getDirectives();
+
+		if (directives) {
+			response.setHeader("Cache-Control", directives);
+		}
+	}
+
+	async setETag (request, response) {
+		var etag = await this.getETag(request, response);
+
+		if (typeof etag !== "undefined" && etag !== null) {
+			response.setHeader("ETag", quote(etag));
+		}
+	}
+
+	async validate (request, response) {
+		var valid;
+
+		if (request.hasHeader("If-None-Match")) {
+			valid = this.eTagIsValid(request, response);
+		} else if (request.hasHeader("If-Modified-Since")) {
+			valid = Date.parse(request.getHeader("If-Modified-Since")) >= await this.getLastModified(request, response);
+		}
+
+		if (valid) {
+			response.send({
+				status: "Not Modified",
+				body: null
+			});
+		}
+	}
+
+	getDirectives () {
+		var directives = [];
+
+		if (this.options.noStore) {
+			directives.push("no-store");
+		}
+
+		if (this.options.noCache) {
+			directives.push("no-Cache");
+		}
+
+		if (this.options.mustRevalidate) {
+			directives.push("must-revalidate");
+		}
+
+		if (this.options.public) {
+			directives.push("public");
+		} else if (this.options.private) {
+			directives.push("private");
+		}
+
+		if (!isNaN(this.options.maxAge)) {
+			directives.push("max-age=" + this.options.maxAge);
+		}
+
+		return directives.join(", ");
+	}
+
+	getETag () {
+		// Child class should implement this method to use ETag cache validation
+	}
+
+	eTagIsValid (request, response) {
+		return response.hasHeader("ETag") && this.getCachedETags(request).find(this.getRawETag(response));
+	}
+
+	getLastModified () {
+		// Child class should implement this method to use modified time weak cache validation
+		return Infinity;
+	}
+
+	getCachedETags (request) {
+		return request.getHeader("If-None-Match").split(",").map(toRawETag)
+	}
+
+	getResponseRawETag (response) {
+		return toRawETag(response.getHeader("ETag"));
+	}
 }
 
-module.exports = CacheControl.create;
-module.exports.CacheControl = CacheControl;
+function quote (string) {
+	if (!string.includes('"')) {
+		string = '"' + string + '"';
+	}
+
+	return string;
+}
+
+function toRawETag (etag) {
+	return etag.trim().replace(quotedETag, "$1");
+}
+
+module.exports = CacheControl;
