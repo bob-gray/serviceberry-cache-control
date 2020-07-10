e8107c0df65fab00ac05d5ba55f3aef04ddef3ae
Bob Gray <gray.bob98@gmail.com>
2020-07-10 10:01:58 -0500

    improving tests

--- spec/plugin.js
+++ spec/plugin.js
@@ -1,9 +1,6 @@
 "use strict";
 
-const CacheControl = require("../plugin"),
-	Request = require("serviceberry/src/Request"),
-	{HttpError} = require("serviceberry"),
-	httpMocks = require("node-mocks-http");
+const CacheControl = require("../plugin");
 
 describe("serviceberry-cache-control", () => {
 	var handler,
@@ -19,29 +16,136 @@ describe("serviceberry-cache-control", () => {
 	it("should create a handler instance with a use() method", () => {
 		expect(typeof handler.use).toBe("function");
 	});
+
+	it("should not set ETag if getETag is not implemented", async () => {
+		await handler.use(request, response);
+
+		expect(response.getHeader("ETag")).toBeUndefined();
+	});
+
+	it("should not send 304 Not Modified when no If- request headers", async () => {
+		await handler.use(request, response);
+
+		expect(response.send).not.toHaveBeenCalled();
+	});
+
+	it("should not set a Cache-Control header if not directives are set", async () => {
+		await handler.use(request, response);
+
+		expect(response.getHeader("Cache-Control")).toBeUndefined();
+	});
+
+	it("should set a Cache-Control header", async () => {
+		handler = new CacheControl({
+			noStore: true
+		});
+
+		await handler.use(request, response);
+
+		expect(response.getHeader("Cache-Control")).toBe("no-store");
+	});
+
+	it("should send a 304 Not Modified if the ETag is valid", async () => {
+		const etag = '"b164c5cd761ad61:0"';
+
+		handler.getETag = () => etag;
+		request = createRequest({
+			"If-None-Match": etag
+		});
+
+		await handler.use(request, response);
+
+		expect(response.send).toHaveBeenCalledWith({
+			status: "Not Modified",
+			body: undefined
+		});
+	});
+
+	it("should not send a 304 Not Modified if the ETag is invalid", async () => {
+		const etag = '"b164c5cd761ad61:0"';
+
+		handler.getETag = () => etag;
+		request = createRequest({
+			"If-None-Match": '"some:other:etag"'
+		});
+
+		await handler.use(request, response);
+
+		expect(response.send).not.toHaveBeenCalled();
+	});
+
+	it("should send a 304 Not Modified if the Last-Modified matches If-Modified-Since", async () => {
+		const lastModified = new Date("Sat, 27 Jun 2020 18:05:33 GMT");
+
+		handler.getLastModified = () => lastModified;
+		request = createRequest({
+			"If-Modified-Since": lastModified.toUTCString()
+		});
+
+		await handler.use(request, response);
+
+		expect(response.send).toHaveBeenCalledWith({
+			status: "Not Modified",
+			body: undefined
+		});
+	});
+
+	it("should send a 304 Not Modified if the Last-Modified is valid", async () => {
+		const lastModified = new Date("Sat, 27 Jun 2020 18:05:33 GMT");
+
+		handler.getLastModified = () => lastModified;
+		request = createRequest({
+			"If-Modified-Since": "Sat, 28 Jun 2020 12:00:00 GMT"
+		});
+
+		await handler.use(request, response);
+
+		expect(response.send).toHaveBeenCalledWith({
+			status: "Not Modified",
+			body: undefined
+		});
+	});
+
+	it("should not send a 304 Not Modified if the Last-Modified is invalid", async () => {
+		const lastModified = new Date();
+
+		handler.getLastModified = () => lastModified;
+		request = createRequest({
+			"If-Modified-Since": "Sat, 27 Jun 2020 18:05:33 GMT"
+		});
+
+		await handler.use(request, response);
+
+		expect(response.send).not.toHaveBeenCalled();
+	});
 });
 
-function createRequest (method = "GET", headers = {}) {
-	var incomingMessage = httpMocks.createRequest({
-			method: method,
-			url: "/",
-			headers: headers
-		}),
-		request;
+function createRequest (headers = {}) {
+	const request = jasmine.createSpyObj("Request", [
+		"hasHeader",
+		"getHeader"
+	]);
 
-
-	incomingMessage.setEncoding = Function.prototype;
-	request = new Request(incomingMessage);
-	request.proceed = jasmine.createSpy("request.proceed");
+	request.hasHeader.and.callFake(header => header in headers);
+	request.getHeader.and.callFake(header => headers[header]);
 
 	return request;
 }
 
 function createResponse () {
-	var response = jasmine.createSpyObj("Response", [
-		"setHeader",
-		"getHeader"
-	]);
+	const response = jasmine.createSpyObj("Response", [
+			"setHeader",
+			"getHeader",
+			"hasHeader",
+			"send"
+		]),
+		headers = {};
+
+	response.setHeader.and.callFake((name, value) => {
+		headers[name] = value;
+	});
+	response.hasHeader.and.callFake(header => header in headers);
+	response.getHeader.and.callFake(header => headers[header]);
 
 	return response;
 }
